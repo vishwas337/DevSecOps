b74574b90c7d0fb23bed2bd706bfc2c941678c7f
Vishwas Goud Sunkari <vishwassunkari41@gmail.com>
2025-01-22 15:47:40 +0530

    new changes in the file to test the git commit

--- spring-boot-cicd/src/java/com/Vishwas/controller/GreetingController.java
+++ spring-boot-cicd/src/java/com/Vishwas/controller/GreetingController.java
@@ -1,13 +1,14 @@
-package java.com.Vishwas.controller;
+package com.Vishwas.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class GreetingController {
     @GetMapping("/")
     public String greeting(Model model) {
         model.addAttribute("message", "Hello from Spring Boot & CI/CD!");
         return "index";
-    
+    }
 }
